4eda469e930c194dd6346891ada93f2346e27ec1
Bijaya775 <73423650+Bijaya775@users.noreply.github.com>
2020-10-26 17:01:51 +0530

    john changes

--- first-project/src/App.java
+++ first-project/src/App.java
@@ -3,6 +3,7 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("hello john changes");
 		System.out.println("hello first repo"+"change in first commit");
 
 	}
